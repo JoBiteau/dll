7492abf6d0b040d629a5c80c7f163456a0c70892
Jo <joffrey.biteau@viacesi.fr>
2020-03-17 15:51:11 +0100

    +service

---
+++ Services/Connect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib1.Services
+{
+    class Connect
+    {
+        private static Connect instance = null;
+        public List<string> Contacts { get; set;}
+
+        private Connect()
+        {
+            Contacts.Add("Barack Afritt");
+            Contacts.Add("Barbara Dégout");
+            Contacts.Add("Bart Haba");
+            Contacts.Add("Bernadette Dejeu");
+        }
+
+        public static Connect Instance
+        {
+            get
+            {
+                if (null == instance)
+                {
+                    instance = new Connect();
+                }
+                return instance;
+            }
+        }
+
+        public List<string> Load()
+        {
+            return Contacts;
+        }
+
+    }
+}
