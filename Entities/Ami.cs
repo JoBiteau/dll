5568982da8101f09701ccc6564a4cfaaa1ad4f73
Jo <joffrey.biteau@viacesi.fr>
2020-03-17 15:21:23 +0100

    first commit

---
+++ Entities/Ami.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteque
+{
+    class Ami : Personne
+    {
+        public DateTime Anniversaire { get; set; }
+        public int Num_mobile { get; set; }
+        public string toString()
+        {
+            return $"{Prenom} {Nom} - anniversaire : {Anniversaire} - {Num_mobile}";
+        }
+    }
+}
---
+++ Entities/Personne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteque
+{
+    class Personne
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; } 
+    }
+}
